df1149906006cd73973091215583f168a4f512d3
Paul Mazak <pmazak2002@yahoo.com>
2012-11-18 21:01:14 -0500

    placeholder for config page

---
+++ src/test/java/org/joust/ConfigServletTest.java
@@ -0,0 +1,17 @@
+package org.joust;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+
+
+public class ConfigServletTest {
+
+	//@Test
+	public void testSetHead() throws FailingHttpStatusCodeException, MalformedURLException, IOException
+	{
+		//new ConfigServlet().setHead("http://yahoo.com");
+	}
+
+}
